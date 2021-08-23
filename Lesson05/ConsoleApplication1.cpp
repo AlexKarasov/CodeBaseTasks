2112c9a8e6933c3261157fe4a0d7e47131cd43b3
Oleksandr Karasov <68482964+OleksandrKarasovCorp@users.noreply.github.com>
2021-08-23 17:42:14 +0300

    Add files via upload

---
+++ Lesson05/ConsoleApplication1.cpp
@@ -0,0 +1,51 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	const int lengthOfArray = 10;
+	int arrayOfNumbers[lengthOfArray] = {};
+
+	std::cout << "Input ten numbers to fill the array, please " << '\n';
+
+	for (int numberToInput = 0; numberToInput < lengthOfArray; numberToInput++) {
+		std::cin >> arrayOfNumbers[numberToInput];
+	}
+
+	std::cout << "The array contains such numbers " << '\n';
+
+	for (int numberToInput = 0; numberToInput < lengthOfArray; numberToInput++) {
+		std::cout << arrayOfNumbers[numberToInput]<<'\t';
+		
+	}
+
+    std::cout << '\n';
+
+
+
+	for (int i = 0; i < lengthOfArray; i++) {
+		for (int j = 0; j < lengthOfArray-1; j++) {
+			if (arrayOfNumbers[j] < arrayOfNumbers[j + 1]) {
+				std::swap(arrayOfNumbers[j], arrayOfNumbers[j + 1]);
+			}
+		}
+	}
+
+
+
+	std::cout << "The array contains such numbers after sorting " << '\n';
+
+	for (int numberToInput = 0; numberToInput < lengthOfArray; numberToInput++) {
+		std::cout << arrayOfNumbers[numberToInput]<<'\t';
+	}
+
+
+
+
+	return 0; 
+
+}
+
+
+
+
