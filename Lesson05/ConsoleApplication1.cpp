528de6dc3ac9aee4e3059a61028c85919191f540
Oleksandr Karasov <68482964+OleksandrKarasovCorp@users.noreply.github.com>
2021-08-23 17:06:08 +0300

    Array sorting

---
+++ Lesson05/ConsoleApplication1.cpp
@@ -0,0 +1,50 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	int arrayOfNumbers[10] = {};
+
+	std::cout << "Input ten numbers to fill the array, please " << '\n';
+
+	for (int numberToInput = 0; numberToInput < 10; numberToInput++) {
+		std::cin >> arrayOfNumbers[numberToInput];
+	}
+
+	std::cout << "The array contains such numbers " << '\n';
+
+	for (int numberToInput = 0; numberToInput < 10; numberToInput++) {
+		std::cout << arrayOfNumbers[numberToInput]<<'\t';
+		
+	}
+
+    std::cout << '\n';
+
+
+
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (arrayOfNumbers[j] < arrayOfNumbers[j + 1]) {
+				std::swap(arrayOfNumbers[j], arrayOfNumbers[j + 1]);
+			}
+		}
+	}
+
+
+
+	std::cout << "The array contains such numbers after sorting " << '\n';
+
+	for (int numberToInput = 0; numberToInput < 10; numberToInput++) {
+		std::cout << arrayOfNumbers[numberToInput]<<'\t';
+	}
+
+
+
+
+	return 0; 
+
+}
+
+
+
+
