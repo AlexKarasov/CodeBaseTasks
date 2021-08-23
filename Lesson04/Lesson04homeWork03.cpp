d1d29a510c010d3dc9f8c7169e812068b7ba3eb0
Oleksandr Karasov <68482964+OleksandrKarasovCorp@users.noreply.github.com>
2021-08-23 17:01:17 +0300

    Scalable house using for loops
    Version 2.0

--- Lesson04/Lesson04homeWork03.cpp
+++ Lesson04/Lesson04homeWork03.cpp
@@ -1,67 +1,74 @@
-﻿
-using namespace std;
-#include <iostream>
+﻿#include <iostream>
 
 int main()
 {
+    const int roof_level = 5;
+    int height;
+
+    next_try:
+
+    std::cout << "Input the height of building, please " << std::endl;
+    std::cin >> height;
+
+    for (int roof_level_counter = 0; roof_level_counter < roof_level * height; roof_level_counter++) {
+        for (int roof_left_space = roof_level * height - roof_level_counter; roof_left_space > 0; roof_left_space--) {
+            std::cout << " ";
+        }
+        std::cout << "/";
+        if (roof_level_counter != roof_level * height - 2 && roof_level_counter != roof_level * height - 1) {
+            for (int roof_middle_space = roof_level_counter * 2; roof_middle_space > 0; roof_middle_space--) {
+                std::cout << " ";
+
+            }
+        }
+        else if (roof_level_counter == roof_level * height - 2) {
+            for (int roof_middle_space = roof_level_counter * 2; roof_middle_space > 0; roof_middle_space--) {
+                std::cout << "-";
+
+            }
+
+        }
+        else if (roof_level_counter == roof_level * height - 1) {
+            std::cout << " |";
+            
+            for (int roof_middle_space = roof_level_counter * 2; roof_middle_space > 4; roof_middle_space--) {
+                std::cout << " ";
+            }
+            std::cout << "| ";
+        }
 
 
-	int l = 0;
-	for (int i = 0; i < 4; i++) {
+        std::cout << "\\" << std::endl;
 
-		for (int j = 5; j - i > 0; j--) {
-			std::cout << ' ';
-		}
-		std::cout << '/';
+    }
+    
+    int wall_level = 7 * height;
 
-		if (i == 3) {
-			for (int m = 0; m < 6; m++) {
-				std::cout << '-';
-			}
-		}
-		else {
-			for (int k = 0; k < l; k++) {
+    for (int wall_level_counter = 0; wall_level_counter < wall_level; wall_level_counter++) {
 
-				std::cout << ' ';
-			}
-		}
-		l += 2;
-		std::cout << '\\' << '\n';
-		}
+        for (int wall_left_space = 0; wall_left_space < 3; wall_left_space++) {
+            std::cout << " ";
+        }
 
-	std::cout << " /";
+        std::cout << "|";
+        if (wall_level_counter != wall_level-1) {
+            for (int wall_middle_space = 0; wall_middle_space < ((roof_level * height) -3 ) * 2; wall_middle_space++) {
 
+                std::cout << " ";
+            }
+        }
+        else {
+            for (int wall_middle_space = 0; wall_middle_space < ((roof_level * height) - 3) * 2; wall_middle_space++) {
 
-	for (int i = 0; i < 7; i++) {
+                std::cout << "_";
+            }
+        }
 
+        std::cout << "|" << std::endl;
 
+    }
 
-
-		if (i == 0) {
-			std::cout << " |";
-			for (int n = 0; n < 4; n++) {
-				std::cout << ' ';
-			}
-			std::cout << "|";
-			std::cout << " \\"<<'\n';
-		}
-		else {
-
-			std::cout << "   |";
-			for (int b = 0; b < 4; b++) {
-				std::cout << ' ';
-			}
-
-			std::cout << '|' << '\n';
-		}
-	}
-	std::cout << "   |";
-	for (int v = 0; v < 4; v++) {
-		std::cout << '_';
-	}
-	std::cout << '|' << '\n';
-
-
-		return 0;
+    goto next_try;
 }
+
 
