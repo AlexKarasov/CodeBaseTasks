b2e6fac0821e3e8bf6ff2476be1d00f6223f03ee
Oleksandr Karasov <68482964+OleksandrKarasovCorp@users.noreply.github.com>
2021-08-25 10:31:17 +0300

    Add files via upload

---
+++ Lesson06/task01/Lesson06_homeWork05.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>                      
+
+int main()                                  
+{			
+
+	const int length = 6;
+
+	int array[length] = { 10, 2, -1, 4, 8, -7 };
+
+	for (int arrayCounter = 0; arrayCounter < length; arrayCounter++) {
+
+		std::cout << *(array + arrayCounter) <<'\t';
+
+	}
+
+	std::cout << std::endl;
+
+	for (int reverse = 0; reverse < length/2; reverse++) {
+
+		int buffer = *(array + reverse);
+		*(array + reverse) = *(array + length - 1 - reverse);             
+		*(array + length - 1 - reverse) = buffer;
+
+	}
+
+	for (int output = 0; output < length; output++) {
+
+		std::cout << *(array + output) <<'\t';
+
+	}
+	
+	return 0;								
+}                                           
+
+
+
+
