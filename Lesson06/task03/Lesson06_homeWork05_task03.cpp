0b2796774a54ee44380167510d566628f1dfd550
Oleksandr Karasov <68482964+OleksandrKarasovCorp@users.noreply.github.com>
2021-08-26 18:23:43 +0300

    Add files via upload

---
+++ Lesson06/task03/Lesson06_homeWork05_task03.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>     
+#include <cmath>
+
+int main()
+{
+  
+   
+    std::cout << "Input the current coordinates of Knight"<<std::endl;
+    
+    std::cout << "x1 = " << std::endl;
+
+    int x1;
+    std::cin >> x1;
+
+    std::cout << "y1 = " << std::endl;
+
+    int y1;
+    std::cin >> y1;
+
+    std::cout << "Input the next coordinates of Knight" << std::endl;
+
+    std::cout << "x2 = " << std::endl;
+
+    int x2;
+    std::cin >> x2;
+
+    std::cout << "y2 = " << std::endl;
+
+    int y2;
+    std::cin >> y2;
+
+
+    int dx = abs(x1 - x2);
+    int dy = abs(y1 - y2);
+
+    if (dx == 1 && dy == 2 || dx == 2 && dy == 1) {
+        std::cout << "Yes.";
+    }
+    else {
+        std::cout << "No.";
+    }
+
+   
+    return 0;
+
+}
+
+
+
+
