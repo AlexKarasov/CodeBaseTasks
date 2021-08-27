03de5dbdade0f464f0e3daaf9208f7916bb0fb86
Oleksandr Karasov <68482964+OleksandrKarasovCorp@users.noreply.github.com>
2021-08-27 15:53:02 +0300

    Add files via upload

---
+++ Lesson07/task02/lesson07_homeWork06 _task02.cpp
@@ -0,0 +1,37 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+	const int lenght = 9;
+
+	int flatArray[lenght] = { -2, 1, - 3, 4, - 1, 2, 1, - 5, 4 };
+
+	std::cout << "The initial sequence is: \n" << std::endl;
+
+	for (int initialOutput = 0; initialOutput < lenght; initialOutput++) {
+		std::cout << flatArray[initialOutput] << '\t';
+	}
+
+	std::cout << '\n' << std::endl;
+
+	int maxSubSeq = flatArray[0];
+	int sum = 0;
+
+	for (int sumCounter = 0; sumCounter < lenght; sumCounter++) {
+		sum += flatArray[sumCounter];
+		maxSubSeq = std::max(maxSubSeq, sum);
+		sum += std::max(sum, 0);
+	}
+	
+	std::cout << "The max sum of subsequence is: \n" << std::endl;
+	std::cout << maxSubSeq;
+	
+
+	return 0;
+}
+
+
+
+
