35495cb1a0571d64da6f2a7fc418d5a2dc50047e
Oleksandr Karasov <68482964+AlexKarasov@users.noreply.github.com>
2021-09-08 18:21:00 +0300

    Update homeWork10.cpp

--- Lesson11/homeWork10.cpp
+++ Lesson11/homeWork10.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+
 struct Node {
     Node* next;
     int data;
@@ -30,10 +31,32 @@ void DeleteNode(List* list) {
     Node* newNode = list->head;
     list->head = newNode->next;
     delete newNode;
-    
-
 }
 
+Node* FindNode(List* list, int data) {
+    for (Node* current = list->head; current != nullptr; current = current->next) {
+        if (current->data == data) {
+            std::cout << "Value of found node: " << current->data << std::endl;
+            return current;
+            break;
+        } 
+        
+    }
+    
+}
+
+void AddNodeAfter(Node* node, int data) {
+
+    if (node == nullptr) {
+        std::cout << "Given Node can not be null" << std::endl;
+   }
+
+    Node* nextNode = new Node;
+    nextNode->data = data;
+    nextNode->next = node->next;
+    node->next = nextNode;
+
+}
 
 int main()
 {
@@ -42,12 +65,19 @@ int main()
     AddNode(&list, 10);
     AddNode(&list, 20);
     AddNode(&list, 30);
+    Node* find = FindNode(&list, 20);
+
+
     ShowList(&list);
     std::cout << "____________________________________________________________________________\n" << std::endl;
     DeleteNode(&list);
     ShowList(&list);
     std::cout << "____________________________________________________________________________\n" << std::endl;
     DeleteNode(&list);
+    ShowList(&list);
+
+    AddNodeAfter(list.head, 8);
+
     ShowList(&list);
 
     return 0;
